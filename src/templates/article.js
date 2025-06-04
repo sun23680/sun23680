0e7016bb76a6a1171d09b882cbe7fb71d535d680
sun23680 <sun23680@naver.com>
2025-06-05 01:02:52 +0900

    Create article.js

---
+++ src/templates/article.js
@@ -0,0 +1,33 @@
+// src/templates/article.js
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/Layout"
+import "../styles/article.css" // 필요하다면 별도 CSS 추가
+
+export default function ArticleTemplate({ data }) {
+  const post = data.markdownRemark
+  return (
+    <Layout>
+      <article className="article-container">
+        <h1 className="article-title">{post.frontmatter.title}</h1>
+        <p className="article-date">{post.frontmatter.date}</p>
+        <div
+          className="article-content"
+          dangerouslySetInnerHTML={{ __html: post.html }}
+        />
+      </article>
+    </Layout>
+  )
+}
+
+export const pageQuery = graphql`
+  query ArticleBySlug($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        date(formatString: "YYYY.MM.DD")
+      }
+      html
+    }
+  }
+`
