372423e51d83217ab92fab9a802092533c83c28f
sun23680 <sun23680@naver.com>
2025-06-04 23:38:35 +0900

    Create gatsby-node.js

---
+++ gatsby-node.js
@@ -0,0 +1,16 @@
+const path = require("path")
+const { createFilePath } = require("gatsby-source-filesystem")
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
+
+  if (node.internal.type === "MarkdownRemark") {
+    const slug = createFilePath({ node, getNode, basePath: "content/news" })
+
+    createNodeField({
+      node,
+      name: "slug",
+      value: slug,
+    })
+  }
+}
