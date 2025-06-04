50cca31ed430041ffe17d7a2029ca3bc591c175e
sun23680 <sun23680@naver.com>
2025-06-04 21:59:21 +0900

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -43,7 +43,7 @@ export default IndexPage
 
 export const pageQuery = graphql`
   query {
-    allMarkdownRemark(sort: {frontmatter: {date: DESC}}) {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
       nodes {
         id
         frontmatter {
